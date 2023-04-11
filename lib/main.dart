10901efedd2283ea4f827a62d9bc88bff254b451
KamleshKasambe <kasambe.kamlesh1@gmail.com>
2023-04-12 01:37:19 +0530

    Time to Play some Quiz

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,38 @@ class Quizzer extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black26,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                flex: 5,
+                child: Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Center(
+                    child: Text(
+                      "This is were the question text will go.",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsets.all(15),
+                  child: TextButton(onPressed: () {}, child: ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
