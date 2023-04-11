105d6c337668aa414c6a4a49508cb807b6df4788
KamleshKasambe <kasambe.kamlesh1@gmail.com>
2023-04-12 01:16:37 +0530

    Time to Play some Quiz

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Quizzer());
+}
+
+class Quizzer extends StatelessWidget {
+  const Quizzer({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
